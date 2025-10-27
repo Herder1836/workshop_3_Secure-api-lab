0f33c8401a35ce5fc6ee5d80794e31d953c3a3d2
Andrei_Pogonia <gretyerty@gmail.com>
2025-10-27 16:08:25 +0200

    feat: Implement basic API endpoints and in-memory data

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const { documents, employees } = require('./data');
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World! The server is running.');
@@ -8,4 +10,22 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Список документів
+app.get('/documents', (req, res) => {
+  res.status(200).json(documents);
+});
+
+// Додати новий документ
+app.post('/documents', (req, res) => {
+  const newDoc = req.body;
+  newDoc.id = Date.now();
+  documents.push(newDoc);
+  res.status(201).json(newDoc);
+});
+
+// Список співробітників
+app.get('/employees', (req, res) => {
+  res.status(200).json(employees);
 });
